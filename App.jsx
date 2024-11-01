ee48a88f3cf19dfbe72d1c966a49f7f145545c19
dongchen <166428054+ding-dong-qiang@users.noreply.github.com>
2024-11-01 10:43:11 -0600

    lab3

--- App.jsx
+++ App.jsx
@@ -5,15 +5,22 @@
  */
 
 import React from 'react';
+import { useState } from 'react';
 import DoForm from './ToDoForm';
 import DoList from './ToDoList';
 import { SafeAreaView} from 'react-native';
 
 
 function App() {
+  const [tasks, setTasks] = useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+
   return (
     <SafeAreaView>
-      <DoList />
+      <DoList tasks = {tasks}/>
       <DoForm />
     </SafeAreaView>
   );
